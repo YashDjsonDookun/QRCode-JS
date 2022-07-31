b197aaf91cb229eb2f7efbb248e8f3dc42a266fc
YashDjsonDookun <dookun88@gmail.com>
2022-07-31 17:32:55 +0400

    Fetch All Users

--- server/index.js
+++ server/index.js
@@ -2,11 +2,28 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan')
-const mysql = require('mysql');
-const PORT = 5000 | process.env.PORT;
+const sql = require("msnodesqlv8");
+const PORT = process.env.PORT | 5000;
+const conn = process.env.CONNECTION_STRING;
 const app = express();
 
-app.use(morgan);
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('QR_GEN Api')
+});
+
+// Fetch all users
+app.get('/users', (req, res)=>{
+    const query = `SELECT * FROM TBL_USER_INFO;`;
+    sql.query(conn, query, (err, users) => {
+        res.send(users)
+    });
+});
+
+app.listen(PORT, ()=> {
+   console.warn(`Listening on http://localhost:${PORT}`) ;
+});
